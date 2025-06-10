f59c02db3e081069e5c8b41d4bc8f6aa1e42174f
Vicente Mataix Ferrándiz <vmataix@altair.com>
2025-06-10 09:36:42 +0200

    Revert quaternion

--- kratos/utilities/quaternion.h
+++ kratos/utilities/quaternion.h
@@ -41,7 +41,7 @@ namespace Kratos
 		/**
 		Creates a Zero Quaternion.
 		*/
-		Quaternion() : mQuaternionValues(boost::numeric::ublas::zero_vector<T>(4)) {
+		Quaternion() : mQuaternionValues(zero_vector<T>(4)) {
 		}
 
 		/**
