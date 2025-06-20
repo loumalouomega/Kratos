484dfa5513c1d0c15bdb0729ac6ac4c897c52286
Vicente Mataix Ferrándiz <vmataix@altair.com>
2025-06-20 21:45:10 +0000

    Refactor mdspan header to improve type aliasing and documentation clarity

--- kratos/containers/mdspan.h
+++ kratos/containers/mdspan.h
@@ -14,99 +14,140 @@
 
 // System includes
 #include <iostream>
+#include <cstddef>
 
 // External includes
-#include <mdspan/mdspan.hpp>
+#include <mdspan.hpp>
 
 // Project includes
 
 namespace Kratos::Future {
 
-/// @name Standard mdspan component aliases
-/// @{
+/**
+ * @file mdspan.h
+ * @brief Provides type aliases and utilities for `std::mdspan`.
+ * @details This header introduces a set of standardized aliases for the core
+ *          components of `std::mdspan`, such as layouts, accessors, and extents.
+ *          It also defines convenient aliases for common `mdspan` types (e.g.,
+ *          1D and 2D views) to simplify their usage throughout the Kratos codebase.
+ *          A stream insertion operator for easy debugging and inspection of `mdspan`
+ *          instances is also included.
+ */
+
+///@name mdspan Component Aliases
+///@{
 
 /**
- * @brief Alias for std::layout_left.
+ * @brief Alias for a left-to-right (column-major) layout mapping.
  * @see https://en.cppreference.com/w/cpp/container/mdspan/layout_left
  */
-using layout_left = std::layout_left;
+using layout_left = MDSPAN_IMPL_STANDARD_NAMESPACE::layout_left;
 
 /**
- * @brief Alias for std::layout_right.
+ * @brief Alias for a right-to-left (row-major) layout mapping.
  * @see https://en.cppreference.com/w/cpp/container/mdspan/layout_right
  */
-using layout_right = std::layout_right;
+using layout_right = MDSPAN_IMPL_STANDARD_NAMESPACE::layout_right;
 
 /**
- * @brief Alias for std::layout_stride.
+ * @brief Alias for a generalized strided layout mapping.
  * @see https://en.cppreference.com/w/cpp/container/mdspan/layout_stride
  */
-using layout_stride = std::layout_stride;
+using layout_stride = MDSPAN_IMPL_STANDARD_NAMESPACE::layout_stride;
 
 /**
- * @brief Alias for std::default_accessor.
- * @see https://en.cppreference.com/w/cpp/container/mdspan/default_accessor
+ * @brief Alias for the default accessor policy, which provides element access via `operator[]`.
+ * @tparam T The element type being accessed.
  */
-template <class T>
-using default_accessor = std::default_accessor<T>;
+template <typename T>
+using default_accessor = MDSPAN_IMPL_STANDARD_NAMESPACE::default_accessor<T>;
 
 /**
- * @brief Alias for std::extents, representing compile-time extents.
- * @see https://en.cppreference.com/w/cpp/container/mdspan/extents
+ * @brief Alias for compile-time extents of an `mdspan`.
+ * @tparam IndexType The type used for indexing.
+ * @tparam Extents A parameter pack of compile-time dimension extents.
  */
-template <class IndexType, std::size_t... Extents>
-using extents = std::extents<IndexType, Extents...>;
+template <typename IndexType, std::size_t... Extents>
+using extents = MDSPAN_IMPL_STANDARD_NAMESPACE::extents<IndexType, Extents...>;
 
 /**
- * @brief Alias for std::dextents, representing run-time extents.
- * @see https://en.cppreference.com/w/cpp/container/mdspan/dextents
+ * @brief Alias for run-time (dynamic) extents of an `mdspan`.
+ * @tparam IndexType The type used for indexing.
+ * @tparam Rank The number of dimensions (rank) of the `mdspan`.
  */
-template <class IndexType, std::size_t Rank>
-using dextents = std::dextents<IndexType, Rank>;
+template <typename IndexType, std::size_t Rank>
+using dextents = MDSPAN_IMPL_STANDARD_NAMESPACE::dextents<IndexType, Rank>;
 
-/// @}
-/// @name Common mdspan type aliases
-/// @{
-
-/**
- * @brief A non-owning view with dynamic extents for a given rank.
- *
- * @tparam T The type of the elements.
- * @tparam Rank The number of dimensions.
- * @tparam Layout The layout mapping (default: layout_right).
- */
-template <class T, std::size_t Rank, class Layout = layout_right>
-using mdspan_view = std::mdspan<T, dextents<std::size_t, Rank>, Layout>;
+///@}
+///@name Common mdspan Type Aliases
+///@{
 
 /**
- * @brief A non-owning 1D view, conceptually similar to std::span.
- * @tparam T The type of the elements.
+ * @brief A non-owning, multi-dimensional view over a contiguous sequence of objects.
+ * @details This is the primary alias for `std::mdspan`, configured with custom
+ * layout and accessor policies. It serves as a versatile, non-owning handle
+ * to multi-dimensional data.
+ * @tparam ElementType The type of elements in the view.
+ * @tparam Extents An `extents` object specifying the dimensions.
+ * @tparam LayoutPolicy The memory layout policy (e.g., `layout_right`).
+ * @tparam AccessorPolicy The policy for accessing elements.
  */
-template <class T>
+template <
+    typename ElementType,
+    typename Extents,
+    typename LayoutPolicy = layout_right,
+    typename AccessorPolicy = default_accessor<ElementType>
+>
+using mdspan = MDSPAN_IMPL_STANDARD_NAMESPACE::mdspan<
+    ElementType,
+    Extents,
+    LayoutPolicy,
+    AccessorPolicy
+>;
+
+/**
+ * @brief A non-owning `mdspan` view with dynamic extents for a specified rank.
+ * @tparam T The type of elements in the view.
+ * @tparam Rank The number of dimensions (rank).
+ * @tparam Layout The memory layout policy (default is row-major).
+ */
+template <typename T, std::size_t Rank, typename Layout = layout_right>
+using mdspan_view = mdspan<T, dextents<std::size_t, Rank>, Layout>;
+
+/**
+ * @brief A non-owning 1D view, conceptually equivalent to `std::span`.
+ * @tparam T The type of elements in the view.
+ */
+template <typename T>
 using mdspan_1d_view = mdspan_view<T, 1>;
 
 /**
  * @brief A non-owning 2D view, conceptually a view of a matrix.
- * @tparam T The type of the elements.
+ * @tparam T The type of elements in the view.
  */
-template <class T>
+template <typename T>
 using mdspan_2d_view = mdspan_view<T, 2>;
 
-/// @}
-/// @name Stream Operators
-/// @{
+///@}
+///@name Stream Operators
+///@{
 
 /**
- * @brief A utility to print the extents and basic info of an mdspan.
+ * @brief Prints a summary of an `mdspan` to an output stream.
+ * @details This operator provides a concise representation of an `mdspan`, including
+ * its rank and the extent of each dimension. This is useful for debugging.
+ * @param rOStream The output stream.
+ * @param rData The `mdspan` to print.
+ * @return The output stream.
  */
-template <class T, class E, class L, class A>
-std::ostream& operator<<(std::ostream& rOStream, const std::mdspan<T, E, L, A>& rData)
+template <typename ElementType, typename Extents, typename LayoutPolicy, typename AccessorPolicy>
+std::ostream& operator<<(std::ostream& rOStream, const mdspan<ElementType, Extents, LayoutPolicy, AccessorPolicy>& rData)
 {
-    rOStream << "mdspan with extents: (";
+    rOStream << "mdspan(rank=" << rData.rank() << ", extents=[";
     for (std::size_t i = 0; i < rData.rank(); ++i) {
         rOStream << rData.extent(i) << (i == rData.rank() - 1 ? "" : ", ");
     }
-    rOStream << ")";
+    rOStream << "])";
     return rOStream;
 }
 
