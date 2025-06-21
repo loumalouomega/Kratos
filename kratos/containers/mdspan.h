fba8efe739e286acfd697dd9020e803d491ca62f
Vicente Mataix Ferrándiz <vmataix@altair.com>
2025-06-21 05:33:30 +0000

    Add alias for full extent in mdspan to support dynamic sizing

--- kratos/containers/mdspan.h
+++ kratos/containers/mdspan.h
@@ -78,6 +78,13 @@ using extents = MDSPAN_IMPL_STANDARD_NAMESPACE::extents<IndexType, Extents...>;
 template <typename IndexType, std::size_t Rank>
 using dextents = MDSPAN_IMPL_STANDARD_NAMESPACE::dextents<IndexType, Rank>;
 
+/**
+ * @brief Alias for a full extent in an `mdspan`, allowing for dynamic sizing.
+ * @details This is used to specify that a dimension should take the full size of the underlying data.
+ * @see https://en.cppreference.com/w/cpp/container/mdspan/full_extent
+ */
+using full_extent = MDSPAN_IMPL_STANDARD_NAMESPACE::full_extent;
+
 ///@}
 ///@name Common mdspan Type Aliases
 ///@{
